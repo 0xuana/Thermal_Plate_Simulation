6a078e0abeb9c4fdd7ae5e7ebf325dd09c2d000f
0xuana <lingxuan@stonybrook.edu>
2023-09-18 20:33:37 -0400

    Complete the third function

--- HW2.m
+++ HW2.m
@@ -3,7 +3,7 @@
 n = 50;
 plate = initializePlate(n);
 
-% Visualize and save the initial temperature distribution
+% Visualize and save the initial temperature distribution   
 % imagesc(plate);
 % colorbar;
 % colormap(cool);
@@ -109,6 +109,18 @@ function matrix = updateTemperature(input)
         end
     end
     matrix = re;
+end
+
+function [avgTemp, maxChangePoint] = analyzePlate(initialPlate, finalPlate)
+    avgTemp = sum(finalPlate, 1:2) / (size(finalPlate, 1) * size(finalPlate, 1));
+    plateChange = abs(initialPlate - finalPlate);
+   
+    maxD1 = max(plateChange, [], 1);        % Get the maximum in every column
+    maxD2 = max(plateChange, [], 2);        % Get the maximum in every rows
+    [~,i_c] = max(maxD1);               % Find the maximum number in the column and store it column index in i_c 
+    [~,i_r] = max(maxD2);               % Find the maximum number int the row and store it row index in i_r
+
+    maxChangePoint = [i_r, i_c];
 end
 
 function heat_map(plate)
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Thermal_Plate_Simulation
 Project written as an AMS 325 TA.
-
-
-<img alt="Initial plate" src="https://github.com/0xuana/Thermal_Plate_Simulation/blob/main/initial_temperature_hw2.png" width=600px style="display: block; margin: auto;"/>
